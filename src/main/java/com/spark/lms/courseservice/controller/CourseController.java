23a676a6d3af11287f4c5b70835c28ee50deda4d
Rajath MG <rajath.mg@encora.com>
2025-11-27 20:28:08 +0530

    CRUD operations

--- src/main/java/com/spark/lms/courseservice/controller/CourseController.java
+++ src/main/java/com/spark/lms/courseservice/controller/CourseController.java
@@ -1,7 +1,10 @@
 package com.spark.lms.courseservice.controller;
 
+import com.spark.lms.courseservice.dto.CourseDTO;
 import com.spark.lms.courseservice.dto.StudentCourseDTO;
+import com.spark.lms.courseservice.entity.Course;
 import com.spark.lms.courseservice.service.CourseService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +20,66 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    
     @GetMapping("/enrolledCourses")
     public ResponseEntity<List<StudentCourseDTO>> getEnrolledCourses(
             @RequestHeader("userId") Long studentId,
             @RequestHeader("role") String role
     ) {
-        
+
         if (!"STUDENT".equalsIgnoreCase(role)) {
             return ResponseEntity.status(403).build();
         }
 
         List<StudentCourseDTO> enrolledCourses = courseService.getEnrolledCourses(studentId);
         return ResponseEntity.ok(enrolledCourses);
+    }
+
+    
+    @PostMapping
+    public ResponseEntity<Course> addCourse(
+            @RequestBody CourseDTO dto,
+            @RequestHeader("userId") Long creatorId,
+            @RequestHeader("role") String role
+    ) {
+        if (!role.equals("TEACHER") && !role.equals("ADMIN")) {
+            return ResponseEntity.status(403).build();
+        }
+
+        return ResponseEntity.ok(courseService.addCourse(dto, creatorId));
+    }
+
+    // GET COURSE BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        return ResponseEntity.ok(courseService.getCourseById(id));
+    }
+
+    // UPDATE COURSE
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateCourse(
+            @PathVariable Long id,
+            @RequestBody CourseDTO dto,
+            @RequestHeader("role") String role
+    ) {
+        if (!role.equals("TEACHER") && !role.equals("ADMIN")) {
+            return ResponseEntity.status(403).build();
+        }
+
+        return ResponseEntity.ok(courseService.updateCourse(id, dto));
+    }
+
+    // DELETE COURSE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(
+            @PathVariable Long id,
+            @RequestHeader("role") String role
+    ) {
+        if (!role.equals("ADMIN")) {
+            return ResponseEntity.status(403).build();
+        }
+
+        courseService.deleteCourse(id);
+        return ResponseEntity.ok().build();
     }
 }
